acb74d9f11dfc4379829da270ebafd7675a888c0
Mark Eschbach <mark@rhumbix.com>
2016-06-10 09:17:12 -0700

    Allows environment overriding cflags while building

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from distutils.core import setup, Extension
 import numpy.distutils.misc_util
+import os
+
+if "PYROVER_CFLAGS" in os.environ:
+    native_cflags = os.environ["PYROVER_CFLAGS"].split(" ")
+else:
+    native_cflags = ["-std=c++1y"]
+
+print "[Rover] Building native extensions using " + str(native_cflags)
 
 setup(
-    ext_modules=[Extension("_rover", ["py_module.cc"], extra_compile_args=["-std=c++1y"])],
+    ext_modules=[Extension("_rover", ["py_module.cc"], extra_compile_args=native_cflags)],
     include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs()
 )
